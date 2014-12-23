7b625efab9cc1778b08645ac2f4aaeb6db996831
macraris <macraris@gmail.com>
2014-12-24 00:09:29 +0100

    add JavaScript-1.js

---
+++ JavaScript-1.js
@@ -0,0 +1,23 @@
+//Eloquent Javascript site book
+//http://eloquentjavascript.net/chapter2.html
+
+var evalAris=prompt("Hello Chiaretta !? let's play!","Quanto fa 2+2");
+
+while (isNaN(evalAris)){
+  alert("pls insert a number");
+  evalAris=prompt("Sei Furba Chiaretta!? aha aah","Quanto fa 2+2");
+}
+ ///* 
+if(evalAris==4){
+		alert("eh Chiaretta, Sarai sorpresa ma hai sbagliato!!!");
+  			alert("Ta dan!");
+}else if(evalAris==3 || evalAris== 5){
+		alert("Forse ragioni gia' un po'");
+   } else if (evalAris===null){
+        alert("che pigrona pero'--> hai risposto zero");
+    } else {
+              alert("brava--> Complimenti!"+ "/br" + "Alleluah!");
+              }
+    //*/
+    
+    
